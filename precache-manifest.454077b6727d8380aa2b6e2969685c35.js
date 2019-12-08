f6d914aff1f3ca44fe06bc4165d2a4f02161ff85
Linh <linhgraphic@gmail.com>
2019-12-08 02:03:00 +0000

    Updates

---
+++ precache-manifest.454077b6727d8380aa2b6e2969685c35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62553c1fcf148ba94097bc413fbf8a40",
+    "url": "/my-calculator/index.html"
+  },
+  {
+    "revision": "b6d243f2c492f9350649",
+    "url": "/my-calculator/static/css/main.40a84d16.chunk.css"
+  },
+  {
+    "revision": "bbdeb802903b3669ca64",
+    "url": "/my-calculator/static/js/2.274732c9.chunk.js"
+  },
+  {
+    "revision": "b6d243f2c492f9350649",
+    "url": "/my-calculator/static/js/main.a2311182.chunk.js"
+  },
+  {
+    "revision": "f79630093e6723870f97",
+    "url": "/my-calculator/static/js/runtime~main.b960bbd9.js"
+  }
+]);
